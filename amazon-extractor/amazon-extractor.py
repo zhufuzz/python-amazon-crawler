26b4316177696b28dc02d279cf12c5223c24f1d8
wd890415 <wd890415@gmail.com>
2012-08-29 04:26:38 +0000

    add __init__.py to make a package

--- amazon-extractor/amazon-extractor.py
+++ amazon-extractor/amazon-extractor.py
@@ -7,10 +7,13 @@ A specific extractor for Amazon.com.
 Aimed at extractor product information and comment.
 '''
 
-import sys.argv
+from sys import argv
+import cralwer
 
-def main():
-  print "Hellow word"
   
 if __name__ == "__main__":
-  main()
+  if len(argv) >= 2:
+    url = argv[1]
+  else:
+    url = "http://www.amazon.com/Samsung-UN46EH6000-46-Inch-1080p-HDTV/dp/B0071O4EKU/ref=pd_ts_zgc_e_3578042011_2?ie=UTF8&s=electronics&pf_rd_p=1367759742&pf_rd_s=right-6&pf_rd_t=101&pf_rd_i=507846&pf_rd_m=ATVPDKIKX0DER&pf_rd_r=1E8N6GPA3MX4607DC1T6"
+    
---
+++ crawler/__init__.py
@@ -0,0 +1,5 @@
+'''
+Created on 2012-8-28
+
+@author: yaoyao
+'''
