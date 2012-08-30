2ade4471434afc89c5243a0e09738e0b82aa2ebd
wd890415 <wd890415@gmail.com>
2012-08-30 07:31:01 +0000

    update department extraction

--- amazon-extractor/amazon-url-selector.py
+++ amazon-extractor/amazon-url-selector.py
@@ -7,8 +7,39 @@ A amazon specific URL selector.
 Manage URL already extracted, extract regularly and URL queue.
 '''
 
-def main():
-  print "Hello world"
+import HTMLParser
+import urllib2
+from bs4 import BeautifulSoup
+from sys import argv
+
+# Extract productions from a search page
+# or classification product page.
+class DepartmentExtract():
+  def __init__(self):
+    self.search_pattern = ['computer parts & components', 'books']
+    self.view_more = ['see more']
+    self.department_url = []
+    return
   
+  def dfs(self, p):
+    if 'name' in dir(p) and p.name == 'a' and p.string and \
+    HTMLParser.HTMLParser().unescape(p.string).lower() in self.search_pattern:
+      url = 'http://www.amazon.com' + p['href']
+      if not url in self.department_url:
+        self.department_url.append(url)
+    if 'contents' in dir(p):
+      for c in p.contents:
+        self.dfs(c)
+
+  def extract(self, msg):
+    soup = BeautifulSoup(msg)
+    self.dfs(soup)
+
 if __name__ == "__main__":
-  main()
+  # Test classification page.
+  url = "http://www.amazon.com/gp/site-directory/ref=sa_menu_fullstore"
+  response = urllib2.urlopen(url, timeout = 5000)
+  msg = response.read()
+  de = DepartmentExtract()
+  de.extract(msg)
+  print de.department_url
